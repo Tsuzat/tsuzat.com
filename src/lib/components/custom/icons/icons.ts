6d084f24a96f7d03551da11f575a10712f209eba
Tsuzat <dev.tsuzat@gmail.com>
2024-08-30 23:14:29 +0530

    add icons and dropdown-menu

--- src/lib/components/custom/icons/icons.ts
+++ src/lib/components/custom/icons/icons.ts
@@ -2,9 +2,11 @@ import Google from './Google.svelte';
 
 import Moon from 'lucide-svelte/icons/moon-star';
 import Sun from 'lucide-svelte/icons/sun';
+import Device from 'lucide-svelte/icons/computer';
 
 export const Icons = {
 	moon: Moon,
 	sun: Sun,
+	device: Device,
 	google: Google
 };
