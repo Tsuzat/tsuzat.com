782f836bb78838256c02218e72386f1d8e4ff0e2
Tsuzat <dev.tsuzat@gmail.com>
2024-08-30 23:15:08 +0530

    remove firebase in +layout.svelte in base URL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,40 @@
 	import '@fontsource-variable/dm-sans';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import { FirebaseApp } from 'sveltefire';
-	import { auth, firestore } from '$lib/firebase';
 	import Navbar from '$lib/components/custom/Navbar.svelte';
+	import Footer from '$lib/components/custom/Footer.svelte';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate(() => {
+		//@ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((fullfill) => {
+			//@ts-ignore
+			document.startViewTransition(() => new Promise(fullfill));
+		});
+	});
 </script>
+
+<svelte:head>
+	<meta name="description" content="Portfolio and personal website of Tsuzat (Alok Singh)" />
+
+	<meta property="og:site_name" content="Tsuzat" />
+	<meta property="og:title" content="Portfolio and personal website of Tsuzat (Alok Singh)" />
+	<meta
+		property="og:description"
+		content="Personal and Portfolio website which contains the professional details about me. The website has some informative blogs in different toppics."
+	/>
+	<meta property="og:type" content="blog and portfolio" />
+	<meta property="og:url" content="https://tsuzat.com" />
+</svelte:head>
 
 <ModeWatcher />
 
-<FirebaseApp {auth} {firestore}>
+<div class="app">
 	<Navbar />
-	<slot />
-</FirebaseApp>
+	<div class="app w-[60%] mx-auto">
+		<slot />
+		<Footer />
+	</div>
+</div>
