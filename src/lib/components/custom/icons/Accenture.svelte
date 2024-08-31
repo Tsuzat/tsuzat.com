be17de0b4f73733408ea272e236881d8df4c7df7
Tsuzat <dev.tsuzat@gmail.com>
2024-08-31 17:55:19 +0530

    add new Icons Accenture, LinkedIn, Email and X

---
+++ src/lib/components/custom/icons/Accenture.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import type { SVGAttributes } from 'svelte/elements';
+
+	type $$Props = SVGAttributes<SVGElement>;
+</script>
+
+<svg role="img" viewBox="0 0 24 24" {...$$restProps}>
+	<path fill="currentColor" d="m.66 16.95 13.242-4.926L.66 6.852V0l22.68 9.132v5.682L.66 24Z" />
+</svg>
--- src/lib/components/custom/icons/icons.ts
+++ src/lib/components/custom/icons/icons.ts
@@ -1,12 +1,22 @@
 import Google from './Google.svelte';
+import Accenture from './Accenture.svelte';
+import Github from './Github.svelte';
+import LinkedIn from './LinkedIn.svelte';
+import X from './X.svelte';
 
 import Moon from 'lucide-svelte/icons/moon-star';
 import Sun from 'lucide-svelte/icons/sun';
 import Device from 'lucide-svelte/icons/computer';
+import Email from './Email.svelte';
 
 export const Icons = {
 	moon: Moon,
 	sun: Sun,
 	device: Device,
-	google: Google
+	google: Google,
+	github: Github,
+	linkedin: LinkedIn,
+	x: X,
+	email: Email,
+	accenture: Accenture
 };
