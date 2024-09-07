407d0f33568c08420de9405f9beaf15f85375ce4
Tsuzat <dev.tsuzat@gmail.com>
2024-09-07 23:18:18 +0530

    add Calendar icon

--- src/lib/components/custom/icons/icons.ts
+++ src/lib/components/custom/icons/icons.ts
@@ -3,15 +3,18 @@ import Accenture from './Accenture.svelte';
 import Github from './Github.svelte';
 import LinkedIn from './LinkedIn.svelte';
 import X from './X.svelte';
+import Email from './Email.svelte';
 
+// Lucide icons
 import Moon from 'lucide-svelte/icons/moon-star';
 import Sun from 'lucide-svelte/icons/sun';
 import Device from 'lucide-svelte/icons/computer';
-import Email from './Email.svelte';
+import CalendarDays from 'lucide-svelte/icons/calendar-days';
 
 export const Icons = {
 	moon: Moon,
 	sun: Sun,
+	calendar: CalendarDays,
 	device: Device,
 	google: Google,
 	github: Github,
