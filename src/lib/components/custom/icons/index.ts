ed42a25fb626ed461a8aeaa55d53b3a1f5f2269a
Tsuzat <dev.tsuzat@gmail.com>
2024-09-08 18:04:33 +0530

    rename icons.ts -> index.ts

--- src/lib/components/custom/icons/index.ts
+++ src/lib/components/custom/icons/index.ts
@@ -10,11 +10,15 @@ import Moon from 'lucide-svelte/icons/moon-star';
 import Sun from 'lucide-svelte/icons/sun';
 import Device from 'lucide-svelte/icons/computer';
 import CalendarDays from 'lucide-svelte/icons/calendar-days';
+import Clock from 'lucide-svelte/icons/clock';
+import Copy from 'lucide-svelte/icons/copy';
 
 export const Icons = {
 	moon: Moon,
 	sun: Sun,
 	calendar: CalendarDays,
+	clock: Clock,
+	copy: Copy,
 	device: Device,
 	google: Google,
 	github: Github,
