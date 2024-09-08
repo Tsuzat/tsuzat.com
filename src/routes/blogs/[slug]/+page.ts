95492bca978be9d5d4d349691ff67698d5461c81
Tsuzat <dev.tsuzat@gmail.com>
2024-09-08 18:16:49 +0530

    remove prerender for post/[slug] pages

--- src/routes/blogs/[slug]/+page.ts
+++ src/routes/blogs/[slug]/+page.ts
@@ -1,16 +1,15 @@
 import type { PageLoad } from './$types';
 import type { MarkdownPost } from '$lib/utils';
 
-export const prerender = true;
 export const ssr = true;
 
 export const load: PageLoad = async ({ params }) => {
-	const slug = params.slug;
+  const slug = params.slug;
 
-	const markDownPost: MarkdownPost = await import(`../../../posts/${slug}.md`);
+  const markDownPost: MarkdownPost = await import(`../../../posts/${slug}.md`);
 
-	return {
-		metadata: markDownPost.metadata,
-		post: markDownPost.default
-	};
+  return {
+    metadata: markDownPost.metadata,
+    post: markDownPost.default
+  };
 };
