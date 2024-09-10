ee99bfe984b25e66616fa0fac899e71ebe118344
Tsuzat <dev.tsuzat@gmail.com>
2024-09-10 21:22:35 +0530

    fix: CopyCode button copies the code now :|

--- src/lib/components/custom/CopyCode.svelte
+++ src/lib/components/custom/CopyCode.svelte
@@ -1,31 +1,27 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
 	import { toast } from 'svelte-sonner';
 
 	let isClicked = false;
+	let CopyCodeElement: HTMLButtonElement;
 
 	function copyCode() {
 		isClicked = true;
-		const copyButton = document.getElementById('copy-code');
-		if (copyButton) {
-			// Next child element is code so copy that to clipboard
-			const code = copyButton.nextElementSibling;
-			if (code && code.textContent) navigator.clipboard.writeText(code.textContent);
+		// Next child element is code so copy that to clipboard
+		const code = CopyCodeElement.parentNode?.nextSibling;
+		console.log(code);
+		if (code && code.textContent) navigator.clipboard.writeText(code.textContent);
 
-			toast.success('Copied Code to clipboard');
-		}
+		toast.success('Copied Code to clipboard');
 		setTimeout(() => {
 			isClicked = false;
 		}, 2000);
 	}
 </script>
 
-<Button class="p-0 h-fit hover:bg-transparent" variant="ghost" id="copy-code" on:click={copyCode}>
+<button class="p-0 h-fit hover:bg-transparent" on:click={copyCode} bind:this={CopyCodeElement}>
 	{#if isClicked}
-		<!-- <Icons.copyCheck class="size-4 text-green-600" /> -->
 		<span class="text-green-500 text-xs">Copied</span>
 	{:else}
-		<!-- <Icons.copy class="size-4" /> -->
 		<span class="text-xs">Copy</span>
 	{/if}
-</Button>
+</button>
