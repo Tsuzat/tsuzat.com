26e1543d1e9880a3e6ac713e204514c9c97cb07b
Tsuzat <dev.tsuzat@gmail.com>
2024-09-12 21:57:41 +0530

    add Footer

---
+++ src/lib/components/custom/Footer.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Icons } from '$lib/components/custom/icons';
+</script>
+
+<footer class="footer flex items-center justify-between my-4 py-4 text-muted-foreground">
+	<div>© Alok "Tsuzat" Singh {new Date().getFullYear()}</div>
+	<div class="flex items-center gap-4">
+		<a href="https://github.com/Tsuzat" target="_blank" class="">
+			<Icons.github class="size-4" />
+		</a>
+		<a href="https://www.linkedin.com/in/alok-singh-tsuzat" target="_blank">
+			<Icons.linkedin class="size-4" />
+		</a>
+		<a href="mailto:contact@tsuzat.com " target="_blank">
+			<Icons.email class="size-4" />
+		</a>
+		<a href="https://x.com/TheTsuzat" target="_blank">
+			<Icons.x class="size-4" />
+		</a>
+	</div>
+</footer>
