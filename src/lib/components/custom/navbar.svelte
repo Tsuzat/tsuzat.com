8a4fca0a77cae09e9edbbea12fd02c21c0bdad64
Tsuzat <dev.tsuzat@gmail.com>
2024-12-26 21:15:47 +0530

    add: Navbar component

---
+++ src/lib/components/custom/navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { page } from '$app/state';
+	import Lottie from '$lib/components/custom/lottie.svelte';
+	import lottieLogo from '$lib/static/logo.json';
+	import Themeswitcher from './themeswitcher.svelte';
+
+	const links = ['about', 'projects', 'blogs'];
+</script>
+
+<nav class="flex w-full items-center justify-between py-2">
+	<a href="/" class="inline-flex items-center gap-4 text-xl font-bold">
+		<Lottie animationData={lottieLogo} class="size-12" />
+		<span class="hidden text-muted-foreground sm:block">TSUZAT</span>
+	</a>
+	<div id="links" class="inline-flex items-center justify-end gap-8">
+		{#each links as link}
+			<a class:current={page.url.pathname === `/${link}`} href="/{link}" class="capitalize">
+				{link}
+			</a>
+		{/each}
+		<Themeswitcher />
+	</div>
+</nav>
+
+<style>
+	a.current {
+		text-decoration: underline;
+	}
+</style>
