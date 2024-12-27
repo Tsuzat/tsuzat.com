599debbedcaa171eb0b6fe2c60453ca40f25c412
Tsuzat <dev.tsuzat@gmail.com>
2024-12-27 15:42:58 +0530

    add: projects +page.svelte and +page.ts

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,31 @@
-<h1 class="text-2xl font-bold">This is a projects page</h1>
+<script lang="ts">
+	import type { Project } from '$lib/utils';
+	import type { PageData } from './$types';
+	import ProjectView from '$lib/components/custom/project.svelte';
+
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
+
+	const projects: Project[] = data.projects;
+</script>
+
+<svelte:head>
+	<title>Projects | Tsuzat</title>
+</svelte:head>
+
+<div>
+	<h1 class="text-2xl font-bold">Projects</h1>
+	<div>
+		{#each projects as project}
+			<ProjectView {project} />
+		{/each}
+	</div>
+	<p>
+		Check More Projects on <a href="https://github.com/Tsuzat" target="_blank" class="text-blue-500"
+			>GitHub</a
+		>
+	</p>
+</div>
---
+++ src/routes/projects/+page.ts
@@ -0,0 +1,34 @@
+export const prerender = true;
+export const ssr = true;
+
+import { getGithubStars, type Project } from '$lib/utils';
+
+export const load = async () => {
+	const projects: Project[] = [
+		{
+			name: 'NeoSolarzined.nvim',
+			description: `A dark and light Neovim theme written in Lua ported from NeoSolarized with better syntax highlighting. 
+                Includes extra themes for Kitty, Alacritty, Wezterm, Konsole and Windows Terminal.`,
+			url: 'https://github.com/Tsuzat/NeoSolarized.nvim',
+			stars: await getGithubStars('Tsuzat', 'NeoSolarized.nvim')
+		},
+		{
+			name: 'Shad Editor',
+			description:
+				'An editor with rich text features and a beautiful UI, provides full flexibility and customizable components ready to copy and paste, made for developers.',
+			url: 'https://shad-editor.tsuzat.com',
+			stars: await getGithubStars('Tsuzat', 'ShadEditor'),
+			badges: ['https://waka-api.dev-tsuzat.workers.dev/Nota']
+		},
+		{
+			name: 'Kanban',
+			description: `Streamline your workflow with suckless, minimal, powerful and feature rich kanban board app. 
+            Drag-and-drop tasks, write extensive notes on tasks, and stay on top of your projects.`,
+			url: 'https://kanban.tsuzat.com',
+			stars: await getGithubStars('Tsuzat', 'kanban')
+		}
+	];
+	return {
+		projects
+	};
+};
