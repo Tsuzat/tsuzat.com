e3d96d41e03bfb76dea7c4816d80ef56b8110d08
Tsuzat <dev.tsuzat@gmail.com>
2024-12-27 12:54:41 +0530

    add: Accenture, Email, Github, Google, Linkedin and Twitter (X) icons

---
+++ src/lib/components/icons/accenture.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		[key: string]: any;
+	}
+
+	let { ...rest }: Props = $props();
+</script>
+
+<svg role="img" viewBox="0 0 24 24" {...rest}>
+	<path fill="currentColor" d="m.66 16.95 13.242-4.926L.66 6.852V0l22.68 9.132v5.682L.66 24Z" />
+</svg>
