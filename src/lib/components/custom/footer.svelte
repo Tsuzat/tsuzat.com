a7e18b9bbc17859c67dcdbf6f50c260f4fb27fea
Tsuzat <dev.tsuzat@gmail.com>
2024-12-27 22:09:57 +0530

    update: add titles to social media links in footer component

--- src/lib/components/custom/footer.svelte
+++ src/lib/components/custom/footer.svelte
@@ -8,16 +8,16 @@
 <footer class="footer my-4 flex items-center justify-between py-4 text-muted-foreground">
 	<div>© Alok "Tsuzat" Singh {new Date().getFullYear() + 1}</div>
 	<div class="flex items-center gap-4">
-		<a href="https://github.com/Tsuzat" target="_blank" class="">
+		<a href="https://github.com/Tsuzat" target="_blank" title="Github Link">
 			<Github class="size-4" />
 		</a>
-		<a href="https://www.linkedin.com/in/alok-singh-tsuzat" target="_blank">
+		<a href="https://www.linkedin.com/in/alok-singh-tsuzat" target="_blank" title="Linkedin Link">
 			<Linkedin class="size-4" />
 		</a>
-		<a href="mailto:contact@tsuzat.com " target="_blank">
+		<a href="mailto:contact@tsuzat.com " target="_blank" title="Email">
 			<Email class="size-4" />
 		</a>
-		<a href="https://x.com/TheTsuzat" target="_blank">
+		<a href="https://x.com/TheTsuzat" target="_blank" title="Twitter/X">
 			<Twitter class="size-4" />
 		</a>
 	</div>
