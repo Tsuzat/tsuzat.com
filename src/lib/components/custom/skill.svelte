c3f5b0a321a7cf06060a616b519836932621e7b6
Tsuzat <dev.tsuzat@gmail.com>
2025-03-29 22:54:32 +0530

    feat: add Skill component for displaying skills with icons and descriptions

---
+++ src/lib/components/custom/skill.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { Skill } from '$lib/utils';
+
+	interface Props {
+		skill: Skill;
+	}
+
+	let { skill }: Props = $props();
+</script>
+
+<div
+	class="border-1 flex w-60 cursor-pointer items-center gap-2 rounded-lg border-border border-opacity-0 bg-muted/25 p-2 transition-all duration-500 hover:border-opacity-100 hover:bg-muted/80 hover:shadow-lg"
+>
+	<div class="size-fit rounded p-2" style={`background-color: ${skill.color}25`}>
+		<skill.icon class="h-6 w-6" />
+	</div>
+	<div>
+		<div class="text-lg font-semibold">{skill.title}</div>
+		<div class="text-sm text-muted-foreground">{skill.description}</div>
+	</div>
+</div>
