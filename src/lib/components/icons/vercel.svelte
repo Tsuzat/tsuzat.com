4c785afb35851f4fd17d0b11742c9cb410afad70
Tsuzat <dev.tsuzat@gmail.com>
2025-03-29 22:54:10 +0530

    feat: add SVG icons for Figma, Git, Svelte, Tailwind, TypeScript, and Vercel

---
+++ src/lib/components/icons/vercel.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface Props {
+		[key: string]: unknown;
+	}
+
+	let { ...restProps }: Props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="128"
+	height="128"
+	viewBox="0 0 128 128"
+	{...restProps}
+>
+	<path d="M64.002 8.576L128 119.424H0Zm0 0" />
+</svg>
