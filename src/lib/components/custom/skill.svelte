1820da0b1e7630fadc9b2415a2cd666ee175bfe5
Tsuzat <dev.tsuzat@gmail.com>
2025-03-30 11:58:49 +0530

    style: enhance Skill component icon with hover scaling effect

--- src/lib/components/custom/skill.svelte
+++ src/lib/components/custom/skill.svelte
@@ -9,10 +9,10 @@
 </script>
 
 <div
-	class="border-1 flex w-60 cursor-pointer items-center gap-2 rounded-lg border-border border-opacity-0 bg-muted/25 p-2 transition-all duration-500 hover:border-opacity-100 hover:bg-muted/80 hover:shadow-lg"
+	class="border-1 group flex w-60 cursor-pointer items-center gap-2 rounded-lg border-border border-opacity-0 bg-muted/25 p-2 transition-all duration-500 hover:border-opacity-100 hover:bg-muted/80 hover:shadow-lg"
 >
 	<div class="size-fit rounded p-2" style={`background-color: ${skill.color}25`}>
-		<skill.icon class="h-6 w-6" />
+		<skill.icon class="h-6 w-6 scale-100 transition-all duration-500 group-hover:scale-110" />
 	</div>
 	<div>
 		<div class="text-lg font-semibold">{skill.title}</div>
