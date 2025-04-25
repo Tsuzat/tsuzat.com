01626ca38ead3238bc907254b57905c268ed181d
Tsuzat <dev.tsuzat@gmail.com>
2025-04-25 15:41:24 +0530

    Add ModeWatcher component and variable font import to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+	import '@fontsource-variable/open-sans';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
 
 {@render children()}
