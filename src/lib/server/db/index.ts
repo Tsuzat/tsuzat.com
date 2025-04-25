6caca38d73c0e7d0f51eed91b27aceca28c878f2
Tsuzat <dev.tsuzat@gmail.com>
2025-04-25 15:41:04 +0530

    Refactor database schema to define blogs, comments, likes, and user tables; enhance user and session types

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,37 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { eq } from 'drizzle-orm';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = neon(env.DATABASE_URL);
 
 export const db = drizzle(client, { schema });
+
+export async function getUserFromGitHubId(githubId: string) {
+	try {
+		const [user] = await db
+			.select({
+				id: schema.user.id,
+				username: schema.user.username,
+				avatarUrl: schema.user.avatarUrl
+			})
+			.from(schema.user)
+			.where(eq(schema.user.id, githubId));
+		return user;
+	} catch (e) {
+		console.error(e);
+		return null;
+	}
+}
+
+export async function createUser(githubId: string, username: string, avatarUrl: string) {
+	const user: schema.User = {
+		id: githubId,
+		username,
+		avatarUrl
+	};
+	await db.insert(schema.user).values(user);
+	return user;
+}
--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,37 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, timestamp } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', {
+export const blogs = pgTable('blogs', {
 	id: serial('id').primaryKey(),
-	age: integer('age')
+	title: text('title'),
+	counts: integer('counts')
 });
+
+export const comments = pgTable('comments', {
+	id: serial('id').primaryKey(),
+	blogId: integer('blog_id').references(() => blogs.id),
+	content: text('content')
+});
+
+export const likes = pgTable('likes', {
+	id: serial('id').primaryKey(),
+	blogId: integer('blog_id').references(() => blogs.id),
+	userId: text('user_id').references(() => user.id)
+});
+
+export const user = pgTable('users', {
+	id: text('github_id').notNull().unique().primaryKey(),
+	username: text('username').notNull().unique(),
+	avatarUrl: text('avatar_url').notNull()
+});
+
+export const session = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
+});
+
+export type Session = typeof session.$inferSelect;
+
+export type User = typeof user.$inferSelect;
