91077e7cc194268431ab5af0ca9773265c8410fd
Tsuzat <dev.tsuzat@gmail.com>
2025-04-26 22:02:26 +0530

    feat: move meta tags into svelte:head for improved SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,8 +7,20 @@
 	let { children } = $props();
 </script>
 
-<ModeWatcher />
+<svelte:head>
+	<meta name="description" content="Portfolio and personal website of Tsuzat (Alok Singh)" />
 
+	<meta property="og:site_name" content="Tsuzat" />
+	<meta property="og:title" content="Portfolio and personal website of Tsuzat (Alok Singh)" />
+	<meta
+		property="og:description"
+		content="Personal and Portfolio website which contains the professional details about me. The website has some informative blogs in different toppics."
+	/>
+	<meta property="og:type" content="blog and portfolio" />
+	<meta property="og:url" content="https://tsuzat.com" />
+</svelte:head>
+
+<ModeWatcher />
 <main class="mx-auto max-w-4xl p-4">
 	<Navbar />
 	{@render children()}
