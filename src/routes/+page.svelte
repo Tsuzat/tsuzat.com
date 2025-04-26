0a78bf84259ba8c131598820e63b07443aed5dd6
Tsuzat <dev.tsuzat@gmail.com>
2025-04-26 22:02:30 +0530

    feat: replace default content with personalized introduction and SEO title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import TextGrow from '$lib/components/TextGrow.svelte';
+</script>
+
+<svelte:head>
+	<title>Tsuzat | Alok Singh</title>
+</svelte:head>
+
+<div>
+	<div class="mt-16">
+		<TextGrow class="my-4 text-3xl font-bold" text="Namaste, Tsuzat here!" />
+		<p class="text-justify">
+			I'm Alok "Tsuzat" Singh, a software developer at Accenture based in Hyderabad, India. I'm also
+			the creator of Edra, NeoSolarized.nvim and many other open source projects.
+		</p>
+	</div>
+</div>
