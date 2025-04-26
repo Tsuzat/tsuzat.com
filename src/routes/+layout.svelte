dc1e56d5592290f9274a5689056c179ea77755fc
Tsuzat <dev.tsuzat@gmail.com>
2025-04-26 20:07:44 +0530

    feat: wrap children in main layout with Navbar for improved structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,14 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import '@fontsource-variable/open-sans';
 	import '../app.css';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
 
-{@render children()}
+<main class="mx-auto max-w-4xl p-4">
+	<Navbar />
+	{@render children()}
+</main>
