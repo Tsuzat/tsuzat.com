e4d72dba1e40ec94df96a85ada4044f014131d82
Tsuzat <dev.tsuzat@gmail.com>
2025-04-26 20:06:18 +0530

    feat: implement Navbar component with animated logo and dropdown menu

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import { cn } from '$lib/utils';
+	import { scale } from 'svelte/transition';
+	import ThemeToggler from './ThemeToggler.svelte';
+	import LottieRender from './LottieRender.svelte';
+	import animationData from '$lib/static/logo.json';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import Icons from './icons';
+	import { goto } from '$app/navigation';
+
+	const links = ['about', 'blogs', 'projects'];
+</script>
+
+<header class="flex items-center justify-between">
+	<a href="/" class="inline-flex items-center gap-2">
+		<LottieRender {animationData} class="size-10" />
+		<span class="font-bold">TSUZAT</span>
+	</a>
+	<div class="flex items-center gap-4">
+		{#each links as link, idx (idx)}
+			{@const isActive = page.url.pathname.startsWith(`/${link}`)}
+			<a
+				data-active={isActive}
+				href={`/${link}`}
+				class={cn(
+					'group relative z-0 hidden rounded-md px-2 py-1 capitalize sm:block',
+					isActive && 'z-[1]'
+				)}
+			>
+				{#if isActive}
+					<div transition:scale class="bg-muted absolute inset-0 -z-10 rounded-lg"></div>
+				{/if}
+				{link}
+			</a>
+		{/each}
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger
+				class="text-muted-foreground inline-flex cursor-pointer items-center gap-2 sm:hidden"
+				title="Navbar Links"
+			>
+				<Icons.plus class="size-4" />
+				<span>Links</span>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content portalProps={{ children: undefined, disabled: true }} class="sm:hidden">
+				{#each links as link, idx (idx)}
+					<DropdownMenu.Item onclick={() => goto(`/${link}`)} class="capitalize">
+						{link}
+					</DropdownMenu.Item>
+				{/each}
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+		<ThemeToggler />
+	</div>
+</header>
