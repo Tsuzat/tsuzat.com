a6bd5d310c9e4a5e588f7d2d1f4399edcf66156f
Tsuzat <dev.tsuzat@gmail.com>
2025-04-26 20:06:22 +0530

    feat: add ThemeToggler component for dark/light mode switching

---
+++ src/lib/components/ThemeToggler.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { toggleMode } from 'mode-watcher';
+	import Icons from './icons';
+</script>
+
+<Button
+	variant="ghost"
+	class="relative size-6 cursor-pointer hover:bg-transparent"
+	onclick={toggleMode}
+>
+	<Icons.sun class="absolute scale-100 transition-all duration-500 dark:scale-0" />
+	<Icons.moon class="absolute scale-0 transition-all duration-500 dark:scale-100" />
+</Button>
