c37ff3214eeb454464426d82217a1b58f794a70f
Tsuzat <dev.tsuzat@gmail.com>
2025-04-27 20:12:04 +0530

    feat: enhance personal introduction with TextHoverCard components for Accenture and NeoSolarized.nvim

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import TextGrow from '$lib/components/custom/TextGrow.svelte';
+	import TextHoverCard from '$lib/components/custom/TextHoverCard.svelte';
+	import Icons from '$lib/components/icons';
 </script>
 
 <svelte:head>
@@ -10,10 +12,51 @@
 	<div class="my-8">
 		<TextGrow class="my-4 text-3xl font-bold" text="Namaste, Tsuzat here!" />
 		<p class="text-justify">
-			Hi, I'm Alok "Tsuzat" Singh — a software developer at Accenture, based in Hyderabad, India.
-			I'm the creator of Edra, NeoSolarized.nvim, and several other open-source projects. Passionate
-			about building and learning, I am currently working on Smart Bug Triage solutions using GenAI
-			within my organization.
+			Hi, I'm Alok "Tsuzat" Singh — a software developer
+			<TextHoverCard href="https://www.accenture.com/" text="@accenture" classText="underline">
+				<div class="flex items-center gap-2">
+					<Icons.accenture class="size-6" />
+					<div>Accenture India</div>
+				</div>
+				<div
+					class="text-muted-foreground inline-flex w-full items-center justify-center gap-2 text-sm"
+				>
+					<Icons.calendar class="size-4" />
+					<span>Aug 2023</span>
+				</div>
+			</TextHoverCard>, based in Hyderabad, India.
+			<br />
+			I'm the creator of
+			<TextHoverCard
+				href="https://edra.tsuzat.com/"
+				text="Edra"
+				classText="underline"
+				class="max-w-80"
+			>
+				<div class="flex items-center gap-2">
+					<img src="https://edra.tsuzat.com/favicon.png" alt="Edra" class="size-6" />
+					<div>Edra</div>
+				</div>
+				<div class="text-muted-foreground text-sm text-wrap">
+					<span>Best Rich Text Editor, made for Svelte Developers with Tiptap </span>
+				</div>
+			</TextHoverCard>&
+			<TextHoverCard
+				href="https://github.com/Tsuzat/NeoSolarized.nvim"
+				text="NeoSolarized.nvim"
+				classText="underline"
+				class="max-w-80"
+			>
+				<div class="flex items-center gap-2">
+					<Icons.neovim class="size-6" />
+					<div>NeoSolarized.nvim</div>
+				</div>
+				<div class="text-muted-foreground text-sm text-wrap">
+					<span>Best Rich Text Editor, made for Svelte Developers with Tiptap </span>
+				</div>
+			</TextHoverCard>
+			NeoSolarized.nvim. Passionate about building and learning, I am currently working on Smart Bug
+			Triage solutions using GenAI within my organization.
 		</p>
 	</div>
 </div>
