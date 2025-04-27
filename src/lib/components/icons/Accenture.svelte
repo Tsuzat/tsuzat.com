1ff3054eeb547f75cf15dca01a5e7ee2b553bfda
Tsuzat <dev.tsuzat@gmail.com>
2025-04-27 20:11:56 +0530

    feat: add Accenture and Neovim icon components with SVG definitions

---
+++ src/lib/components/icons/Accenture.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	interface Props {
+		[key: string]: any;
+	}
+
+	const { ...rest } = $props();
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" {...rest}
+	><path fill="#a600ff" d="m.66 16.95l13.242-4.926L.66 6.852V0l22.68 9.132v5.682L.66 24Z" /></svg
+>
