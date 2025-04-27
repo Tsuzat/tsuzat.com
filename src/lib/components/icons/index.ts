0cd505f06365817cc1e0bfe4eae205cd347c64e8
Tsuzat <dev.tsuzat@gmail.com>
2025-04-27 21:21:59 +0530

    feat: add Email, Github, and LinkedIn icon components with SVG definitions

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,14 +1,21 @@
-import { MoonStar, Sun, Plus, CalendarDays } from '@lucide/svelte';
+import { MoonStar, Sun, Plus, CalendarDays, ArrowUpRight } from '@lucide/svelte';
 import Accenture from './Accenture.svelte';
 import Neovim from './Neovim.svelte';
+import Github from './Github.svelte';
+import LinkedIn from './LinkedIn.svelte';
+import Email from './Email.svelte';
 
 const Icons = {
 	sun: Sun,
 	moon: MoonStar,
 	plus: Plus,
 	calendar: CalendarDays,
+	arrowUpRight: ArrowUpRight,
 	accenture: Accenture,
-	neovim: Neovim
+	neovim: Neovim,
+	github: Github,
+	linkedIn: LinkedIn,
+	email: Email
 };
 
 export default Icons;
