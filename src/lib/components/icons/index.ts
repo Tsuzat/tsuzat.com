0872db550161c601efc4e95d96a30cd68e7f2eba
Tsuzat <dev.tsuzat@gmail.com>
2025-04-27 20:12:00 +0530

    feat: add CalendarDays, Accenture, and Neovim icons to the Icons component

--- src/lib/components/icons/index.ts
+++ src/lib/components/icons/index.ts
@@ -1,9 +1,14 @@
-import { MoonStar, Sun, Plus } from '@lucide/svelte';
+import { MoonStar, Sun, Plus, CalendarDays } from '@lucide/svelte';
+import Accenture from './Accenture.svelte';
+import Neovim from './Neovim.svelte';
 
 const Icons = {
 	sun: Sun,
 	moon: MoonStar,
-	plus: Plus
+	plus: Plus,
+	calendar: CalendarDays,
+	accenture: Accenture,
+	neovim: Neovim
 };
 
 export default Icons;
