801f9a49ee9537007361bf294fa58ca3328d8bf6
Tsuzat <dev.tsuzat@gmail.com>
2025-04-28 18:18:58 +0530

    feat: add CodeInjector and CopyCode components for enhanced code display and copying functionality

---
+++ src/lib/components/custom/CodeInjector.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { mount, onMount } from 'svelte';
+	import CopyCode from './CopyCode.svelte';
+
+	onMount(() => {
+		const preTags = document.querySelectorAll('pre.shiki');
+		preTags.forEach((tag) => {
+			// Tag has code inside
+			let fileName = '';
+			let codeTag = tag.querySelector('code');
+			if (codeTag) {
+				// Code tag has a property "data-file-name" we need to get it
+				fileName = codeTag.dataset.fileName || 'Terminal';
+			}
+
+			// Generate a new div
+			let preTagParent = tag.parentNode;
+			const div = document.createElement('div');
+			div.className =
+				'!border-1 dark:border-border border-muted-foreground/10 rounded overflow-hidden';
+
+			const divTopBar = document.createElement('div');
+			divTopBar.className =
+				'flex items-center justify-between px-4 rounded-t-md border-b border-muted-foreground/[10%] bg-muted/[25%] px-2.5 py-1 font-mono text-xs text-muted-foreground/75';
+
+			// Add one div with class "file name" and content as "filename.ext"
+			const fileNameDiv = document.createElement('div');
+			fileNameDiv.textContent = fileName;
+
+			mount(CopyCode, { target: divTopBar });
+
+			divTopBar.prepend(fileNameDiv);
+
+			if (preTagParent) {
+				preTagParent.replaceChild(div, tag);
+				div.prepend(divTopBar);
+				div.appendChild(tag);
+			}
+		});
+	});
+</script>
+
+<slot />
---
+++ src/lib/components/custom/CopyCode.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	let isClicked = $state(false);
+	let CopyCodeElement: HTMLButtonElement;
+
+	function copyCode() {
+		isClicked = true;
+		// Next child element is code so copy that to clipboard
+		const code = CopyCodeElement.parentNode?.nextSibling;
+		console.log(code);
+		if (code && code.textContent) navigator.clipboard.writeText(code.textContent);
+
+		setTimeout(() => {
+			isClicked = false;
+		}, 2000);
+	}
+</script>
+
+<button class="h-fit p-0 hover:bg-transparent" onclick={copyCode} bind:this={CopyCodeElement}>
+	{#if isClicked}
+		<span class="text-xs text-green-500">Copied</span>
+	{:else}
+		<span class="text-xs">Copy</span>
+	{/if}
+</button>
