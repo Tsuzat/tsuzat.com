0d1d4e7bb83741dbc4a07589c7f81e2c49a2edd0
Tsuzat <dev.tsuzat@gmail.com>
2025-04-28 17:31:59 +0530

    feat: add MarkdownPost and MarkdownPostMetadataAndSlug types for better post management

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from 'clsx';
+import type { ComponentType } from 'svelte';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
+
+export type MarkdownPost = {
+	metadata: {
+		id: string;
+		title: string;
+		imgUrl: string;
+		publishedAt: string;
+		summary: string;
+		production: boolean;
+		readTime: number;
+	};
+	default: ComponentType;
+};
+
+export type MarkdownPostMetadataAndSlug = {
+	slug: string;
+	metadata: MarkdownPost['metadata'];
+};
