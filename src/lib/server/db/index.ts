2cc7c5c9ed80eb4d3371dd782b63bbc082bc855c
Tsuzat <dev.tsuzat@gmail.com>
2025-04-28 18:22:11 +0530

    fix: update database connection to use static DATABASE_URL import

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,12 +1,10 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 import { eq } from 'drizzle-orm';
+import { DATABASE_URL } from '$env/static/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = neon(env.DATABASE_URL);
+const client = neon(DATABASE_URL);
 
 export const db = drizzle(client, { schema });
 
