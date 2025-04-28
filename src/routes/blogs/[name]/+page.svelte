66f9b581eb51a5a3a19ac82585c1e9df4f309af8
Tsuzat <dev.tsuzat@gmail.com>
2025-04-28 18:19:29 +0530

    feat: implement blog page with dynamic code highlighting and blockquote styling

---
+++ src/routes/blogs/[name]/+page.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import type { PageData } from './$types';
+	import { page } from '$app/state';
+	import './+page.css';
+	import CodeInjector from '$lib/components/custom/CodeInjector.svelte';
+
+	interface Props {
+		data: PageData;
+	}
+
+	let { data }: Props = $props();
+
+	onMount(async () => {
+		// Find the code tags
+		const codeTags = document.querySelectorAll('code');
+		codeTags.forEach((codeTag) => {
+			if (codeTag.parentElement && codeTag.parentElement.tagName === 'PRE') return;
+			codeTag.classList.add('bg-muted', 'text-foreground');
+		});
+
+		const blockQuotes = document.querySelectorAll('blockquote');
+		blockQuotes.forEach((blockQuote) => {
+			const tagClass = blockQuote.textContent?.split(':')[0] || '';
+			switch (tagClass) {
+				case 'Warning':
+					blockQuote.classList.add(
+						'!border-l-orange-500/80',
+						'bg-orange-500/[15%]',
+						'text-orange-500'
+					);
+					break;
+				case 'Error':
+					blockQuote.classList.add('!border-l-red-500/80', 'bg-red-500/[15%]', 'text-red-500');
+					break;
+				case 'Info':
+					blockQuote.classList.add(
+						'!border-l-green-500/80',
+						'bg-green-500/[15%]',
+						'text-green-500'
+					);
+					break;
+				default:
+					break;
+			}
+		});
+		// Add ID to all headings
+		const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+		headings.forEach((heading) => {
+			if (!heading.textContent) return;
+			// remove spaces and special characters with regex by replacing them with '-'
+			heading.id = heading.textContent
+				.replace(/[^a-zA-Z0-9 -]/g, '')
+				.replace(/\s+/g, '-')
+				.toLowerCase();
+		});
+	});
+</script>
+
+<svelte:head>
+	<title>{data.metadata.title}</title>
+	<meta name="description" content={data.metadata.summary} />
+	<meta property="og:title" content={data.metadata.title} />
+	<meta property="og:type" content="article" />
+	<meta property="og:description" content={data.metadata.summary} />
+	<meta property="og:image" content={data.metadata.imgUrl} />
+	<meta property="og:url" content={page.url.href} />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<div class="w-full">
+	<h1 class="text-3xl font-bold">{data.metadata.title}</h1>
+	<p class="text-muted-foreground my-2 flex items-center text-sm">
+		<span>
+			{data.metadata.readTime} mins
+		</span>
+	</p>
+	<p class="text-muted-foreground my-2 text-sm">{data.metadata.summary}</p>
+
+	<div class="post prose dark:prose-invert prose-a:to-blue-600 my-4 min-w-full">
+		<CodeInjector>
+			<data.post />
+		</CodeInjector>
+	</div>
+</div>
