4bb43f1d6d91e7d8b7ced86cac95b6bbc6b473d6
Tsuzat <dev.tsuzat@gmail.com>
2025-04-29 11:45:51 +0530

    feat: add PATCH handler for updating comments and improve GET handler formatting

--- src/routes/api/blog/[id]/comments/+server.ts
+++ src/routes/api/blog/[id]/comments/+server.ts
@@ -1,12 +1,17 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
-import { comments } from '$lib/server/db/schema';
+import { comments, type Comment } from '$lib/server/db/schema';
 import { db } from '$lib/server/db';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 
 export const GET: RequestHandler = async ({ params }) => {
 	const { id } = params;
 	if (!id) return new Response(JSON.stringify({ message: 'Id not found' }), { status: 404 });
-	const blogComments = await db.select().from(comments).where(eq(comments.blogId, id)).execute();
+	const blogComments = await db
+		.select()
+		.from(comments)
+		.where(eq(comments.blogId, id))
+		.orderBy(comments.createdAt)
+		.execute();
 	return json({ comments: blogComments }, { status: 200 });
 };
 
@@ -28,6 +33,22 @@ export const PUT: RequestHandler = async ({ params, request }) => {
 				blogId: commentRequest.blogId
 			})
 			.returning();
+		return json({ ...addedComment }, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return json({ message: 'Something went wrong' }, { status: 500 });
+	}
+};
+
+export const PATCH: RequestHandler = async ({ params, request }) => {
+	const { id } = params;
+	if (!id) return new Response(JSON.stringify({ message: 'Id not found' }), { status: 404 });
+	try {
+		const commentRequest: Comment = await request.json();
+		const addedComment = await db
+			.update(comments)
+			.set({ content: commentRequest.content, updatedAt: sql`NOW()` })
+			.where(eq(comments.id, commentRequest.id));
 		return json({ ...addedComment }, { status: 200 });
 	} catch (error) {
 		console.error(error);
