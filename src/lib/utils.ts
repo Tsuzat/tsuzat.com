119c4872c1a1f192f4c700bda5b7840d7fc26b23
Tsuzat <dev.tsuzat@gmail.com>
2025-04-29 18:16:01 +0530

    feat: add Project interface and projects array for managing project details

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,9 @@ import { type ClassValue, clsx } from 'clsx';
 import type { ComponentType } from 'svelte';
 import { twMerge } from 'tailwind-merge';
 
+import neosolarized from '$lib/assets/static/neosolazied.png';
+import edra from '$lib/assets/static/edra.png';
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
@@ -23,3 +26,37 @@ export type MarkdownPostMetadataAndSlug = {
 	slug: string;
 	metadata: MarkdownPost['metadata'];
 };
+
+export interface Project {
+	href: string;
+	img: string;
+	title: string;
+	description: string;
+	github: {
+		username: string;
+		repo: string;
+	};
+}
+
+export const projects: Project[] = [
+	{
+		href: 'https://edra.tsuzat.com/',
+		img: edra,
+		title: 'Edra',
+		description: 'Best Rich Text Editor, made for Svelte Developers with Tiptap',
+		github: {
+			username: 'Tsuzat',
+			repo: 'edra'
+		}
+	},
+	{
+		href: 'https://github.com/Tsuzat/NeoSolarized.nvim',
+		img: neosolarized,
+		title: 'NeoSolarized.nvim',
+		description: 'NeoSolarized colorscheme for NeoVim with full transparency',
+		github: {
+			username: 'Tsuzat',
+			repo: 'NeoSolarized.nvim'
+		}
+	}
+];
