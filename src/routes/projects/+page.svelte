2be91f22f4104bc6bbb6498beab938445ff30f5a
Tsuzat <dev.tsuzat@gmail.com>
2025-04-29 18:16:25 +0530

    feat: replace static header with dynamic project display using Card component

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,1 +1,19 @@
-<h1>Projects</h1>
+<script lang="ts">
+	import Card from '$lib/components/custom/Card.svelte';
+	import TextGrow from '$lib/components/custom/TextGrow.svelte';
+	import { projects } from '$lib/utils';
+</script>
+
+<svelte:head>
+	<title>Blogs | Tsuzat</title>
+</svelte:head>
+
+<TextGrow text="Projects" duration={0.5} class="mt-12 text-2xl font-bold" />
+
+<div class="animate-load-top fill-mode-both mt-4 flex items-center justify-between delay-500">
+	{#each projects as project}
+		<Card {project} />
+	{/each}
+</div>
+
+<p class="mt-8">Expore More...</p>
