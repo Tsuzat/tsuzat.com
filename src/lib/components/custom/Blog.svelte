3008aeb9f0b93ae818aacc2068e2c6bdd6f60685
Tsuzat <dev.tsuzat@gmail.com>
2025-04-29 17:38:42 +0530

    feat: add Blog component for displaying individual blog posts

---
+++ src/lib/components/custom/Blog.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	interface Props {
+		slug: string;
+		title: string;
+		summary: string;
+		publishedAt: string;
+	}
+
+	const { slug, title, summary, publishedAt }: Props = $props();
+
+	import * as Card from '$lib/components/ui/card';
+</script>
+
+<Card.Root
+	onclick={() => goto(`/blogs/${slug}`)}
+	class="bg-background scale-100 cursor-pointer border-0 !p-1 transition-all duration-500 hover:scale-[102%]"
+>
+	<Card.Content class="p-0">
+		<div class="flex items-center gap-4">
+			<span>{publishedAt}</span>
+			<span class="font-bold">{title}</span>
+		</div>
+		<div>
+			<p class="text-muted-foreground text-sm">{summary}</p>
+		</div>
+	</Card.Content>
+</Card.Root>
